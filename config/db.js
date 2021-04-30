49bb05fcdc291322ffe9ddb6c4c88db287b4c376
vishal chintham <chinthamvishal.2019@vitstudent.ac.in>
2021-04-30 16:44:09 +0530

    fix: sockets code also use sequelize

--- config/db.js
+++ config/db.js
@@ -1,20 +1,10 @@
-// const mysql = require('mysql')
-
-
-// const pool = mysql.createPool({
-//   host: process.env.HOSTDB,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   database: process.env.DATABASE
-// })
-
-// module.exports = pool
 require('dotenv').config()
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE,process.env.USER,process.env.PASSWORD,{
   dialect:'mysql',
-  host:process.env.HOSTDB
+  host:process.env.HOSTDB,
+  logging:process.env.TESTING?true:false
 })
 
 module.exports = sequelize;
